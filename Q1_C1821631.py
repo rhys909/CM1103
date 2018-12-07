8ced77d5100cdf1cf27b0dacb199f6a3094462bb
rhys909 <rhyskconnor99@gmail.com>
2018-12-07 01:37:54 +0000

    Q1.b DONE and Q1.c started

--- Q1_C1821631.py
+++ Q1_C1821631.py
@@ -1,13 +1,13 @@
+import csv
 #two variables added for testing
 
-sailor = ("Bob", [2, 4, 1, 1, 2, 5])
+sailor = [("Bob", [2, 4, 1, 1, 2, 5])]
 sailors_in_series = [("Alice", [1, 2, 1, 1, 1, 1]), ("Bob", [3, 1, 5, 3, 2, 5]),
 ("Clare", [2, 3, 2, 2, 4, 2]), ("Dennis", [5, 4, 4, 4, 3, 4]),
 ("Eva", [4, 5, 3, 5, 5, 3])]
 
 def series_score(sailor):
-    #Take the variable of the sailor
-    #take their worst score
+    #Take the variable of the sailor + take their worst score
     max_score = sorted(sailor[1])[-1]
     #Get the sum of their series
     total = sum(sorted(sailor[1])) - max_score
@@ -16,10 +16,18 @@ def series_score(sailor):
 
 def sort_series(sailors_in_series):
     #Get the scores of the sailors
-    for i in sailors_in_series:
-        series_score(sailors_in_series[i])
-    sailors_in_series
     #Sort the scores in ascending order
-
+    sailors_in_series = sorted(sailors_in_series, key=lambda x: (series_score(x), x[1][0]))
     #Return the sorted list
     return sailors_in_series
+
+def read_sailor_data():
+
+    with open('sailor_performance.csv', 'r') as csv_file:
+        performance_dict = {}
+        csv_reader = csv.reader(csv_file)
+
+        next(csv_reader)
+        for line in csv_reader:
+            performance_dict[line[0]] = list(line[1], line[2])
+    return performance_dict
